f1e44fa58a11122f886c3d8fcdc27a10e9560307
Nick Barry <itsnickbarry@protonmail.ch>
2025-05-31 18:51:05 -0600

    use tsx for tests, add empty test suite

---
+++ test/test.ts
@@ -0,0 +1,8 @@
+import assert from 'node:assert';
+import { describe, it } from 'node:test';
+
+describe('todo: test suite', () => {
+  it('todo: test case', async () => {
+    assert(false, 'TODO');
+  });
+});
